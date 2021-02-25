2842dd26a5cd7b256a9e39a7099ff3cbf536aa73
Danilo Vieira <danilo_strvieira@hotmail.com>
2021-02-25 01:36:11 -0300

    Elixir - Aulas 02 e 03

--- elixir/rocketpay/lib/rocketpay_web/views/error_view.ex
+++ elixir/rocketpay/lib/rocketpay_web/views/error_view.ex
@@ -1,5 +1,9 @@
 defmodule RocketpayWeb.ErrorView do
+  import Ecto.Changeset, only: [traverse_errors: 2]
+
   use RocketpayWeb, :view
+
+  alias Ecto.Changeset
 
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
@@ -12,5 +16,17 @@ defmodule RocketpayWeb.ErrorView do
   # "Not Found".
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  def render("404.json", %Changeset{} = changeset) do
+    %{message: translate_errors(changeset)}
+  end
+
+  defp translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
---
+++ elixir/rocketpay/lib/rocketpay_web/views/users_view.ex
@@ -0,0 +1,22 @@
+defmodule RocketpayWeb.UsersView do
+  alias Rocketpay.{User, Account}
+
+  def render("create.json", %{
+        user: %User{
+          id: id,
+          name: name,
+          nickname: nickname,
+          account: %Account{id: account_id, balance: balance}
+        }
+      }) do
+    %{
+      message: "User created",
+      user: %{
+        id: id,
+        name: name,
+        nickname: nickname,
+        account: %{account_id: account_id, balance: balance}
+      }
+    }
+  end
+end
